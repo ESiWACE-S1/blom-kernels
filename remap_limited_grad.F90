369df973b9d54f02ade661bc10c33023d899ac99
David Guibert <david.guibert@gmail.com>
2022-05-05 15:08:01 +0200

    remap_limited_grad: opt3

--- remap_limited_grad.F90
+++ remap_limited_grad.F90
@@ -37,6 +37,10 @@ program remap_zero
   !https://stackoverflow.com/a/6880672
   real(8)::t1,delta, delta_orig
 
+  real :: pbmin_, xd_, yd_, sx_, sy_, sd_, tx_, ty_, td_, dx_, dy_, dp_
+  real :: dp_min, dp_max, t_min, t_max, s_min, s_max
+  real :: te, tw, tn, ts, se, sw, sn, ss
+
   i0 = 0
   ii = idm
   j0 = 0
@@ -419,18 +423,18 @@ program remap_zero
 
               ! --- --- Compute limited gradient for layer pressure thickness and
               ! --- --- center of mass coordinate.
-              dpsw=max(dpeps,min(pbmin(i,j)-pup(isw,jsw),dp(isw,jsw)))
-              dps =max(dpeps,min(pbmin(i,j)-pup(i  ,js ),dp(i  ,js )))
-              dpse=max(dpeps,min(pbmin(i,j)-pup(ise,jse),dp(ise,jse)))
-              dpw =max(dpeps,min(pbmin(i,j)-pup(iw ,j  ),dp(iw ,j  )))
-              dpc =max(dpeps,min(pbmin(i,j)-pup(i  ,j  ),dp(i  ,j  )))
-              dpe =max(dpeps,min(pbmin(i,j)-pup(ie ,j  ),dp(ie ,j  )))
-              dpnw=max(dpeps,min(pbmin(i,j)-pup(inw,jnw),dp(inw,jnw)))
-              dpn =max(dpeps,min(pbmin(i,j)-pup(i  ,jn ),dp(i  ,jn )))
-              dpne=max(dpeps,min(pbmin(i,j)-pup(ine,jne),dp(ine,jne)))
-              dx(i,j)=(dpe-dpw)*dxi
-              dy(i,j)=(dpn-dps)*dyi
-              dgmx=.5*(abs(dx(i,j))+abs(dy(i,j)))
+              pbmin_ = pbmin(i,j)
+              dpsw=max(dpeps,min(pbmin_-pup(isw,jsw),dp(isw,jsw)))
+              dps =max(dpeps,min(pbmin_-pup(i  ,js ),dp(i  ,js )))
+              dpse=max(dpeps,min(pbmin_-pup(ise,jse),dp(ise,jse)))
+              dpw =max(dpeps,min(pbmin_-pup(iw ,j  ),dp(iw ,j  )))
+              dpc =max(dpeps,min(pbmin_-pup(i  ,j  ),dp(i  ,j  )))
+              dpe =max(dpeps,min(pbmin_-pup(ie ,j  ),dp(ie ,j  )))
+              dpnw=max(dpeps,min(pbmin_-pup(inw,jnw),dp(inw,jnw)))
+              dpn =max(dpeps,min(pbmin_-pup(i  ,jn ),dp(i  ,jn )))
+              dpne=max(dpeps,min(pbmin_-pup(ine,jne),dp(ine,jne)))
+              dx_=(dpe-dpw)*dxi
+              dy_=(dpn-dps)*dyi
               ! q1=dx(i,j)*(-.5-xd(i,j))
               ! q2=dx(i,j)*( .5-xd(i,j))
               ! q3=dy(i,j)*(-.5-yd(i,j))
@@ -440,76 +444,351 @@ program remap_zero
               dfmx=max(0.,max(dpsw,dps,dpse,dpw,dpe,dpnw,dpn,dpne)-dpc)
               dfmn=min(0.,min(dpsw,dps,dpse,dpw,dpe,dpnw,dpn,dpne)-dpc)
               if (dfmx.gt.0..and.dfmn.lt.0.) then
+                 dgmx=.5*(abs(dx_)+abs(dy_))
                  q=min(dfmx/max(dfmx,dgmx),dfmn/min(dfmn,-dgmx))
-                 dx(i,j)=dx(i,j)*q
-                 dy(i,j)=dy(i,j)*q
-                 xd(i,j)=dx(i,j)/(12.*dp(i,j))
-                 yd(i,j)=dy(i,j)/(12.*dp(i,j))
+                 dx_=dx_*q
+                 dy_=dy_*q
+                 xd_=dx_/(12.*dp(i,j))
+                 yd_=dy_/(12.*dp(i,j))
               else
                  dx(i,j)=0.
                  dy(i,j)=0.
-                 xd(i,j)=0.
-                 yd(i,j)=0.
+                 xd_=0.
+                 yd_=0.
               endif
+              dx(i,j) = dx_
+              dy(i,j) = dy_
               ! --- --- Compute limited gradients for temperature, salinity, and
               ! --- --- density
-              tx(i,j)=(temp(ie,j)-temp(iw,j))*dxi
-              ty(i,j)=(temp(i,jn)-temp(i,js))*dyi
-              q1=tx(i,j)*(-.5-xd(i,j))
-              q2=tx(i,j)*( .5-xd(i,j))
-              q3=ty(i,j)*(-.5-yd(i,j))
-              q4=ty(i,j)*( .5-yd(i,j))
-              tgmx=max(q1,q2)+max(q3,q4)
-              tgmn=min(q1,q2)+min(q3,q4)
+              tx_=(temp(ie,j)-temp(iw,j))*dxi
+              ty_=(temp(i,jn)-temp(i,js))*dyi
+              td_=temp(i,j)
               tfmx=max(0.,max(temp(isw,jsw),temp(i  ,js ),      &
                               temp(ise,jse),temp(iw ,j  ), &
                               temp(ie ,j  ),temp(inw,jnw), &
                               temp(i  ,jn ),temp(ine,jne)) &
-                          -temp(i,j))
+                          -td_)
               tfmn=min(0.,min(temp(isw,jsw),temp(i  ,js ),      &
                               temp(ise,jse),temp(iw ,j  ), &
                               temp(ie ,j  ),temp(inw,jnw), &
                               temp(i  ,jn ),temp(ine,jne)) &
-                          -temp(i,j))
+                          -td_)
               if (tfmx.gt.0..and.tfmn.lt.0.) then
+                 q1=tx_*(-.5-xd_)
+                 q2=tx_*( .5-xd_)
+                 q3=ty_*(-.5-yd_)
+                 q4=ty_*( .5-yd_)
+                 tgmx=max(q1,q2)+max(q3,q4)
+                 tgmn=min(q1,q2)+min(q3,q4)
                  q=min(tfmx/max(tfmx,tgmx),tfmn/min(tfmn,tgmn))
-                 tx(i,j)=tx(i,j)*q
-                 ty(i,j)=ty(i,j)*q
-                 td(i,j)=temp(i,j)-tx(i,j)*xd(i,j)-ty(i,j)*yd(i,j)
+                 tx_=tx_*q
+                 ty_=ty_*q
+                 td_=td_-tx_*xd_-ty_*yd_
               else
-                 tx(i,j)=0.
-                 ty(i,j)=0.
-                 td(i,j)=temp(i,j)
+                 tx_=0.
+                 ty_=0.
               endif
+              tx(i,j) = tx_
+              ty(i,j) = ty_
+              td(i,j) = td_
 
-              sx(i,j)=(saln(ie,j)-saln(iw,j))*dxi
-              sy(i,j)=(saln(i,jn)-saln(i,js))*dyi
-              q1=sx(i,j)*(-.5-xd(i,j))
-              q2=sx(i,j)*( .5-xd(i,j))
-              q3=sy(i,j)*(-.5-yd(i,j))
-              q4=sy(i,j)*( .5-yd(i,j))
-              sgmx=max(q1,q2)+max(q3,q4)
-              sgmn=min(q1,q2)+min(q3,q4)
+              sx_=(saln(ie,j)-saln(iw,j))*dxi
+              sy_=(saln(i,jn)-saln(i,js))*dyi
+              sd_=saln(i,j)
               sfmx=max(0.,max(saln(isw,jsw),saln(i  ,js ),     &
                                   saln(ise,jse),saln(iw ,j  ), &
                                   saln(ie ,j  ),saln(inw,jnw), &
                                   saln(i  ,jn ),saln(ine,jne)) &
-                             -saln(i,j))
+                             -sd_)
               sfmn=min(0.,min(saln(isw,jsw),saln(i  ,js ),     &
                                   saln(ise,jse),saln(iw ,j  ), &
                                   saln(ie ,j  ),saln(inw,jnw), &
                                   saln(i  ,jn ),saln(ine,jne)) &
-                             -saln(i,j))
+                             -sd_)
               if (sfmx.gt.0..and.sfmn.lt.0.) then
+                 q1=sx_*(-.5-xd_)
+                 q2=sx_*( .5-xd_)
+                 q3=sy_*(-.5-yd_)
+                 q4=sy_*( .5-yd_)
+                 sgmx=max(q1,q2)+max(q3,q4)
+                 sgmn=min(q1,q2)+min(q3,q4)
                  q=min(sfmx/max(sfmx,sgmx),sfmn/min(sfmn,sgmn))
-                 sx(i,j)=sx(i,j)*q
-                 sy(i,j)=sy(i,j)*q
-                 sd(i,j)=saln(i,j)-sx(i,j)*xd(i,j)-sy(i,j)*yd(i,j)
+                 sx_=sx_*q
+                 sy_=sy_*q
+                 sd_=sd_-sx_*xd_-sy_*yd_
               else
-                 sx(i,j)=0.
-                 sy(i,j)=0.
-                 sd(i,j)=saln(i,j)
+                 sx_=0.
+                 sy_=0.
               endif
+              sx(i,j) = sx_
+              sy(i,j) = sy_
+              sd(i,j) = sd_
+           end if
+        end do
+     end do
+     delta=delta+(wallclock()-t1)
+  enddo
+  write(*,*) "done"
+  write(*,'(a,3(f14.6,x))') "timing", delta, delta/real(MAX_ITERATIONS), delta_orig/delta
+#endif
+
+#define REMAP_LIMITED_GRAD_OPT2
+#ifdef REMAP_LIMITED_GRAD_OPT2
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  write(*,*) "opt2: iterating over",MAX_ITERATIONS, " iterations..."
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+  delta=0.0
+  do n_it=1, MAX_ITERATIONS
+     t1=wallclock()
+     ! --- ------------------------------------------------------------------
+     ! --- Compute limited gradients, center of mass coordinates, and
+     ! --- non-dimensional velocities.
+     ! --- ------------------------------------------------------------------
+     !
+     do j=1-mrg-1,jj+mrg+1
+        do i=1-mrg-1,ii+mrg+1
+           if(ip(i,j) /= 0) then
+              ! --- --- Define indices for grid cell neighbors, ensuring that only wet
+              ! --- --- points are used.
+              iw=i-iu(i  ,j)
+              ie=i+iu(i+1,j)
+              dxi=1./max(1,ie-iw)
+
+              js=j-iv(i,j  )
+              jn=j+iv(i,j+1)
+              dyi=1./max(1,jn-js)
+
+              pbmin_ = pbmin(i,j)
+
+              dp_ = max(dpeps,min(pbmin_-pup(i  ,j  ),dp(j  ,j  )))
+              dp_max = dp_
+              dp_min = dp_
+              dpw = dp_
+              dpe = dp_
+              dps = dp_
+              dpn = dp_
+
+              td_ = temp(i  , j  )
+              t_max = td_
+              t_min = td_
+              tw = td_
+              te = td_
+              ts = td_
+              tn = td_
+
+              sd_ = saln(i  , j  )
+              s_max = sd_
+              s_min = sd_
+              sw = sd_
+              se = sd_
+              ss = sd_
+              sn = sd_
+
+              ! iw defined
+              if(iw /= 0) then
+                 if(js /= 0 .and. ip(i-1,j-1) /= 0) then
+                    dp_ = max(dpeps,min(pbmin_-pup(i-1,j-1),dp(i-1,j-1)))
+                    dp_max = max(dp_max, dp_)
+                    dp_min = min(dp_min, dp_)
+
+                    td_ = temp(i-1, j-1)
+                    t_max = max(t_max, td_)
+                    t_min = max(t_min, td_)
+
+                    sd_ = saln(i-1, j-1)
+                    s_max = max(s_max, sd_)
+                    s_min = max(s_min, sd_)
+                 end if
+                 if(              ip(i-1,j  ) /= 0) then
+                    dp_ = max(dpeps,min(pbmin_-pup(i-1,j  ),dp(i-1,j  )))
+                    dp_max = max(dp_max, dp_)
+                    dp_min = min(dp_min, dp_)
+
+                    td_ = temp(i-1, j  )
+                    t_max = max(t_max, td_)
+                    t_min = max(t_min, td_)
+
+                    sd_ = saln(i-1, j  )
+                    s_max = max(s_max, sd_)
+                    s_min = max(s_min, sd_)
+                    ! dpw
+                    dpw = dp_
+                    tw = td_
+                    sw = sd_
+                 end if
+                 if(jn /= 0 .and. ip(i-1,j+1) /= 0) then
+                    dp_ = max(dpeps,min(pbmin_-pup(i-1,j+1),dp(i-1,j+1)))
+                    dp_max = max(dp_max, dp_)
+                    dp_min = min(dp_min, dp_)
+
+                    td_ = temp(i-1, j+1)
+                    t_max = max(t_max, td_)
+                    t_min = max(t_min, td_)
+
+                    sd_ = saln(i-1, j+1)
+                    s_max = max(s_max, sd_)
+                    s_min = max(s_min, sd_)
+                 end if
+              end if
+
+                 if(js /= 0 .and. ip(i  ,j-1) /= 0) then
+                    dp_ = max(dpeps,min(pbmin_-pup(i  ,j-1),dp(j  ,j-1)))
+                    dp_max = max(dp_max, dp_)
+                    dp_min = min(dp_min, dp_)
+
+                    td_ = temp(i-1, j-1)
+                    t_max = max(t_max, td_)
+                    t_min = max(t_min, td_)
+
+                    sd_ = saln(i-1, j-1)
+                    s_max = max(s_max, sd_)
+                    s_min = max(s_min, sd_)
+                    ! dps
+                    dps = dp_
+                    ts = td_
+                    ss = sd_
+                 end if
+
+                 if(jn /= 0 .and. ip(i  ,j+1) /= 0) then
+                    dp_ = max(dpeps,min(pbmin_-pup(i  ,j+1),dp(j  ,j+1)))
+                    dp_max = max(dp_max, dp_)
+                    dp_min = min(dp_min, dp_)
+
+                    td_ = temp(i  , j+1)
+                    t_max = max(t_max, td_)
+                    t_min = max(t_min, td_)
+
+                    sd_ = saln(i  , j+1)
+                    s_max = max(s_max, sd_)
+                    s_min = max(s_min, sd_)
+                    ! dpn
+                    dpn = dp_
+                    tn = td_
+                    sn = sd_
+                 end if
+
+               ! ie defined
+              if(ie /= 0) then
+                 if(js /= 0 .and. ip(i+1,j-1) /= 0) then
+                    dp_ = max(dpeps,min(pbmin_-pup(i+1,j-1),dp(i+1,j-1)))
+                    dp_max = max(dp_max, dp_)
+                    dp_min = min(dp_min, dp_)
+
+                    td_ = temp(i+1, j-1)
+                    t_max = max(t_max, td_)
+                    t_min = max(t_min, td_)
+
+                    sd_ = saln(i+1, j-1)
+                    s_max = max(s_max, sd_)
+                    s_min = max(s_min, sd_)
+                 end if
+                 if(              ip(i+1,j  ) /= 0) then
+                    dp_ = max(dpeps,min(pbmin_-pup(i+1,j  ),dp(i+1,j  )))
+                    dp_max = max(dp_max, dp_)
+                    dp_min = min(dp_min, dp_)
+
+                    td_ = temp(i+1, j  )
+                    t_max = max(t_max, td_)
+                    t_min = max(t_min, td_)
+
+                    sd_ = saln(i+1, j  )
+                    s_max = max(s_max, sd_)
+                    s_min = max(s_min, sd_)
+                    ! dpe
+                    dpe = dp_
+                    te = td_
+                    se = sd_
+                 end if
+                 if(jn /= 0 .and. ip(i+1,j+1) /= 0) then
+                    dp_ = max(dpeps,min(pbmin_-pup(i+1,j-1),dp(i+1,j+1)))
+                    dp_max = max(dp_max, dp_)
+                    dp_min = min(dp_min, dp_)
+
+                    td_ = temp(i+1, j+1)
+                    t_max = max(t_max, td_)
+                    t_min = max(t_min, td_)
+
+                    sd_ = saln(i+1, j+1)
+                    s_max = max(s_max, sd_)
+                    s_min = max(s_min, sd_)
+                 end if
+              end if
+
+              ! --- --- center of mass coordinate.
+              dx_=(dpe-dpw)*dxi
+              dy_=(dpn-dps)*dyi
+              dfmx=max(0.,dp_max-dpc)
+              dfmn=min(0.,dp_min-dpc)
+              if (dfmx.gt.0..and.dfmn.lt.0.) then
+                 ! q1=dx_*(-.5-xd_)
+                 ! q2=dx_*( .5-xd_)
+                 ! q3=dy_*(-.5-yd_)
+                 ! q4=dy_*( .5-yd_)
+                 ! dgmx=max(q1,q2)+max(q3,q4)
+                 ! dgmn=min(q1,q2)+min(q3,q4)
+                 dgmx=.5*(abs(dx_)+abs(dy_))
+                 q=min(dfmx/max(dfmx,dgmx),dfmn/min(dfmn,-dgmx))
+                 dx_=dx_*q
+                 dy_=dy_*q
+                 xd_=dx_/(12.*dp(i,j))
+                 yd_=dy_/(12.*dp(i,j))
+              else
+                 dx(i,j)=0.
+                 dy(i,j)=0.
+                 xd_=0.
+                 yd_=0.
+              endif
+              dx(i,j) = dx_
+              dy(i,j) = dy_
+              ! --- --- Compute limited gradients for temperature, salinity, and
+              ! --- --- density
+              tx_=(te-tw)*dxi
+              ty_=(tn-ts)*dyi
+              td_=temp(i,j)
+              tfmx=max(0.,t_max-td_)
+              tfmn=min(0.,t_min-td_)
+              if (tfmx.gt.0..and.tfmn.lt.0.) then
+                 q1=tx_*(-.5-xd_)
+                 q2=tx_*( .5-xd_)
+                 q3=ty_*(-.5-yd_)
+                 q4=ty_*( .5-yd_)
+                 tgmx=max(q1,q2)+max(q3,q4)
+                 tgmn=min(q1,q2)+min(q3,q4)
+                 q=min(tfmx/max(tfmx,tgmx),tfmn/min(tfmn,tgmn))
+                 tx_=tx_*q
+                 ty_=ty_*q
+                 td_=td_-tx_*xd_-ty_*yd_
+              else
+                 tx_=0.
+                 ty_=0.
+              endif
+              tx(i,j) = tx_
+              ty(i,j) = ty_
+              td(i,j) = td_
+
+              sx_=(se-sw)*dxi
+              sy_=(sn-ss)*dyi
+              sd_=saln(i,j)
+              sfmx=max(0.,s_max-sd_)
+              sfmn=min(0.,s_min-sd_)
+              if (sfmx.gt.0..and.sfmn.lt.0.) then
+                 q1=sx_*(-.5-xd_)
+                 q2=sx_*( .5-xd_)
+                 q3=sy_*(-.5-yd_)
+                 q4=sy_*( .5-yd_)
+                 sgmx=max(q1,q2)+max(q3,q4)
+                 sgmn=min(q1,q2)+min(q3,q4)
+                 q=min(sfmx/max(sfmx,sgmx),sfmn/min(sfmn,sgmn))
+                 sx_=sx_*q
+                 sy_=sy_*q
+                 sd_=sd_-sx_*xd_-sy_*yd_
+              else
+                 sx_=0.
+                 sy_=0.
+              endif
+              sx(i,j) = sx_
+              sy(i,j) = sy_
+              sd(i,j) = sd_
            end if
         end do
      end do
